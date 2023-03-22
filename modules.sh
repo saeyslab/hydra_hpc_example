f49f9ef759a2e9a385e3dc4863484c8f84d9505d
Benjamin Rombaut <benjamin.rombaut@gmail.com>
2023-03-22 18:05:20 +0100

    add more modules

--- modules.sh
+++ modules.sh
@@ -1,5 +1,11 @@
 #!/bin/bash
 
 module load \
-Hydra/1.1.1-GCCcore-10.3.0 \
+Python/3.10.8-GCCcore-12.2.0 \
+Biopython/1.81-foss-2022b \
 
+# https://hpc.vub.be/docs/software/additional_software/#installing-additional-python-packages
+pip install --user hydra-core \
+hydra-submitit-launcher \
+hydra-joblib-launcher \
+memray \
