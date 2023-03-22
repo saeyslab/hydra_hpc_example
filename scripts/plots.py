3391f8162b0fe74fea48a13ef5ed4d433872274b
Benjamin Rombaut <benjamin.rombaut@gmail.com>
2023-03-22 19:24:24 +0100

    only plot when possible

--- scripts/plots.py
+++ scripts/plots.py
@@ -3,12 +3,13 @@ from pathlib import Path
 import subprocess
 import re
 import yaml
+from typing import Any
 
-def parse_runtime(f):
+def parse_runtime(f: Path) -> tuple[int, float]:
     x, y = f.read_text().split(' ')
     return int(x), float(y)
 
-def parse_memory(f):
+def parse_memory(f: Path) -> float | None:
     memory_file = f.parent / 'memory.txt'
     with open(memory_file, 'w') as fh:
         subprocess.run(['python3', '-m', 'memray', 'tree', str(f)], stdout=fh)
@@ -26,46 +27,48 @@ def parse_memory(f):
         case [n, _]:
             return float(n)
 
-def plot_runtime(multirun):
-    files = multirun.glob('**/runtime.txt')
+def plot_runtime(files: list[Path]) -> list[Any]:
     runtimes = [parse_runtime(f) for f in files]
     print(runtimes)
     xs, ys = zip(*runtimes)
     return plt.plot(xs, ys, marker='o')
 
 
-def get_task(f):
+def get_task(f) -> int:
     config_path = (f.parent / '.hydra' / 'config.yaml')
     with open(config_path) as fh:
         config = yaml.safe_load(fh)
     return int(config['task'])
 
-def plot_memory(multirun):
-    files = multirun.glob('**/memray.bin')
+def plot_memory(files) -> plt.Figure: 
     memories = [(get_task(f), parse_memory(f)) for f in files]
     print(memories)
     xs, ys = zip(*memories)
     return plt.plot(xs, ys, marker='o')
 
-def get_sorted_files(p):
+def get_sorted_files(p) -> list[Path]:
     return sorted(p.glob('*'))
 
-def main(multirun, output):
+def main(multirun, output) -> None:
     if multirun is None:
         multirun = get_sorted_files(get_sorted_files(Path('multirun'))[-1])[-1]
     if output is None:
         output = multirun
     print(output)
 
-    _ = plot_runtime(multirun)
-    plt.title('Runtime (s)')
-    plt.savefig(output / 'runtime.png')
-    plt.close()
+    runtime_files = list(multirun.glob('**/runtime.txt'))
+    if runtime_files:
+        _ = plot_runtime(runtime_files)
+        plt.title('Runtime (s)')
+        plt.savefig(output / 'runtime.png')
+        plt.close()
 
-    _ = plot_memory(multirun)
-    plt.title('Memory (MB)')
-    plt.savefig(output / 'memory.png')
-    plt.close()
+    memory_files = list(multirun.glob('**/memray.bin'))
+    if memory_files:
+        _ = plot_memory(memory_files)
+        plt.title('Memory (MB)')
+        plt.savefig(output / 'memory.png')
+        plt.close()
 
 if __name__ == "__main__":
     import argparse
